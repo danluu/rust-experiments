6eff247f17c5320f2663735e00f7bba9cdb5308d
Dan Luu <danluu@gmail.com>
2013-04-03 21:07:51 -0400

    Transfer pointer ownership

--- hello.rs
+++ hello.rs
@@ -2,6 +2,7 @@ fn main() {
     io::println("hello?");
     unique_ptr();
     heap_alloc();
+    transfer_ownership()
 }
 
 fn unique_ptr() {
@@ -18,4 +19,16 @@ fn heap_alloc() {
 fn use_heap_alloc(x: @int) {
     let y: @int = x;
     println(fmt!("use_heap_alloc: %d", *y));   
+}
+
+struct Point {
+    x: int,
+    y: int,
+}
+
+fn transfer_ownership() {
+    let a: ~Point = ~Point { x: 10, y: 20 };
+    let b = a;
+    println(b.x.to_str());
+    // println(a.x.to_str()); //can't access a here
 }
