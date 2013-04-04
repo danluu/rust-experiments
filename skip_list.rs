1e5a4727ebc56ae19e95cb76b6e59260043deec2
Dan Luu <danluu@gmail.com>
2013-04-04 15:07:12 -0400

    New fn: insert_before on linked list

--- skip_list.rs
+++ skip_list.rs
@@ -1,6 +1,8 @@
 struct Node {
-    next: MaybeNode,
-    prev: MaybeNode,
+    left: MaybeNode,
+    right: MaybeNode,
+    down: MaybeNode,
+    up: MaybeNode,
     data: int
 }
 
@@ -34,36 +36,8 @@ fn insert_after(x: @mut Node, y: @mut Node){
     }
 }
 
-// insert node y before node x
-fn insert_before(x: @mut Node, y: @mut Node){
-    match x.prev {
-        SomeNode(xp) => {
-            xp.next = SomeNode(y);
-            x.prev = SomeNode(y);
-            y.prev = SomeNode(xp);
-            y.next = SomeNode(x)
-        }
-        NoNode => {
-            x.prev = SomeNode(y);
-            y.next = SomeNode(x);
-        }
-    }
-}
-
-fn insert_balanced(x: @mut Node){
-    match x.next {
-        SomeNode(xn) => {
-            
-        }
-        NoNode => {
-
-        }
-    }
-}
-
 fn main() {
     let node100 = @mut Node { next: NoNode, prev: NoNode, data: 100 };
-    let node150 = @mut Node { next: NoNode, prev: NoNode, data: 150 };
     let node200 = @mut Node { next: NoNode, prev: NoNode, data: 200 };
     let node250 = @mut Node { next: NoNode, prev: NoNode, data: 250 };
     let node300 = @mut Node { next: NoNode, prev: NoNode, data: 300 };
@@ -71,7 +45,6 @@ fn main() {
     insert_after(node100, node200);
     insert_after(node200, node300);
     insert_after(node200, node250);
-    insert_before(node200, node150);
 
     print_nodes(node100);
 }
