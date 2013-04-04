77803707bf01167f11580978c2b1a459884b7178
Dan Luu <danluu@gmail.com>
2013-04-03 20:32:41 -0400

    Try using smart pointers

--- hello.rs
+++ hello.rs
@@ -1,3 +1,21 @@
 fn main() {
-  io::println("hello?");
+    io::println("hello?");
+    unique_ptr();
+    heap_alloc();
+}
+
+fn unique_ptr() {
+    let x: ~int = ~1024;
+    println(fmt!("unique_ptr: %d", *x));
+}
+
+fn heap_alloc() {
+    let x: @int = @512;
+    use_heap_alloc(x);
+    println(fmt!("heap_alloc: %d", *x));   
+}
+
+fn use_heap_alloc(x: @int) {
+    let y: @int = x;
+    println(fmt!("use_heap_alloc: %d", *y));   
 }
